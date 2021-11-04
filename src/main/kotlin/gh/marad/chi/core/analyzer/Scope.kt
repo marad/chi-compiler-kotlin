6bf475a67785ca80761cb2a07058fa3c769d9f22
Marcin Radoszewski <moriturius@gmail.com>
2021-11-04 17:45:17 +0100

    Remove separate functions namespace from Scope

--- src/main/kotlin/gh/marad/chi/core/analyzer/Scope.kt
+++ src/main/kotlin/gh/marad/chi/core/analyzer/Scope.kt
@@ -3,16 +3,11 @@ package gh.marad.chi.core.analyzer
 import gh.marad.chi.core.*
 
 class Scope(private val parentScope: Scope? = null) {
-    private val functions = mutableMapOf<String, Fn>()
     private val variables = mutableMapOf<String, Expression>()
     private val functionParams = mutableMapOf<String, Type>()
 
     fun defineVariable(name: String, value: Expression) {
         variables[name] = value
-    }
-
-    fun defineFunction(name: String, value: Fn) {
-        functions[name] = value
     }
 
     fun defineFunctionParams(params: List<FnParam>) {
@@ -27,31 +22,28 @@ class Scope(private val parentScope: Scope? = null) {
             ?: parentScope?.findVariable(name, location)
             ?: throw RuntimeException("There is no variable '${name}' in scope. Error at ${location?.formattedPosition}")
 
-    fun findFunction(name: String, location: Location?): Fn =
-        // TODO Better exception
-        functions[name]
-            ?: parentScope?.findFunction(name, location)
-            ?: throw RuntimeException("There is no function '$name' in scope. Error at ${location?.formattedPosition}")
-
+    // TODO Better exceptions
+    fun findFunction(name: String, location: Location?): Fn {
+        val function = findVariable(name, location)
+        if (function is Fn) {
+            return function
+        } else {
+            throw RuntimeException("Variable '$name' is not a function")
+        }
+    }
 
     companion object {
         fun fromExpressions(expression: List<Expression>, parentScope: Scope? = null): Scope {
             val scope = Scope(parentScope)
             expression.forEach { expr ->
                 when(expr) {
-                    is Assignment -> {
-                        when (expr.value) {
-                            is Fn -> scope.defineFunction(expr.name, expr.value)
-                            else -> scope.defineVariable(expr.name, expr.value)
-                        }
-                    }
+                    is Assignment -> scope.defineVariable(expr.name, expr.value)
                     is Atom -> {}
                     is BlockExpression -> {}
                     is Fn -> {}
                     is FnCall -> {}
                     is VariableAccess -> {}
                 }
-
             }
             return scope
         }
