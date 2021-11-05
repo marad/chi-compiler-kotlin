84cff59aa61886f4da8361bd41758518016fd78d
Marcin Radoszewski <moriturius@gmail.com>
2021-11-05 08:11:03 +0100

    Fix failing test

--- src/test/kotlin/gh/marad/chi/core/analyzer/TypeInferenceSpec.kt
+++ src/test/kotlin/gh/marad/chi/core/analyzer/TypeInferenceSpec.kt
@@ -1,6 +1,7 @@
 package gh.marad.chi.core.analyzer
 
 import gh.marad.chi.core.*
+import gh.marad.chi.core.Type.Companion.i32
 import gh.marad.chi.core.Type.Companion.unit
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
@@ -10,20 +11,20 @@ class TypeInferenceSpec : FunSpec() {
         fun asts(code: String): List<Expression> = parse(tokenize(code))
         fun ast(code: String): Expression = asts(code).last()
         test("should read type for simple atom") {
-            inferType(Scope(), ast("5")).shouldBe(Type.i32)
+            inferType(Scope(), ast("5")).shouldBe(i32)
         }
 
         test("should read type from definition") {
-            inferType(Scope(), ast("val x: i32 = 0")).shouldBe(Type.i32)
+            inferType(Scope(), ast("val x: i32 = 0")).shouldBe(i32)
         }
 
         test("should infer type from assigned expression expression") {
-            inferType(Scope(), ast("val x = 0")).shouldBe(Type.i32)
+            inferType(Scope(), ast("val x = 0")).shouldBe(i32)
         }
 
         test("should infer type form accessing variable in scope")  {
             val scope = Scope.fromExpressions(asts("val x = 10"))
-            inferType(scope, ast("x")).shouldBe(Type.i32)
+            inferType(scope, ast("x")).shouldBe(i32)
         }
 
         test("block type is depends on it's last expression") {
@@ -31,7 +32,7 @@ class TypeInferenceSpec : FunSpec() {
                 fn() {}
                 14
             """.trimIndent()))
-            inferType(Scope(), block).shouldBe(Type.i32)
+            inferType(Scope(), block).shouldBe(i32)
         }
 
         test("empty block is of type 'unit'") {
@@ -48,24 +49,23 @@ class TypeInferenceSpec : FunSpec() {
                     val foo = fn(): i32 { 5 }
                 """.trimIndent()))
             inferType(scope, ast("main()")).shouldBe(unit)
-            inferType(scope, ast("foo()")).shouldBe(Type.i32)
+            inferType(scope, ast("foo()")).shouldBe(i32)
         }
 
         test("function calls should use external names") {
             val scope = Scope()
-            scope.defineExternalName("ext", Type.i32)
+            scope.defineExternalName("ext", i32)
 
-            inferType(scope, ast("ext()")).shouldBe(Type.i32)
+            inferType(scope, ast("ext()")).shouldBe(i32)
         }
 
         test("locally defined names should shadow external ones") {
             val scope = Scope()
-            scope.defineExternalName("foo", unit)
-            // TODO: tutaj jest problem - inferType() powinno zwrócić i32, ale tak na prawdę typ "foo" to jest 'fn(): i32'
-            // chyba trzeba wreszcie rozkminić ten typ zmiennej dla funkcji
-            scope.defineVariable("foo", ast("5"))
+            scope.defineExternalName("foo", Type.fn(i32))
+            scope.defineVariable("foo", ast("fn(x: i32) {}"))
 
-            inferType(scope, ast("foo()")).shouldBe(Type.i32)
+            inferType(scope, ast("foo()")).shouldBe(unit)
+            inferType(scope, ast("foo")).shouldBe(Type.fn(returnType = unit, i32))
         }
     }
 }
