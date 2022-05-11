2d03ec60a92613f66db4fbd158a8cb83e38d621f
Marcin Radoszewski <moriturius@gmail.com>
2022-05-11 19:17:09 +0200

    Cache function returning node

--- chi-truffle-language/src/main/java/gh/marad/chi/truffle/nodes/expr/ReadVariableExpr.java
+++ chi-truffle-language/src/main/java/gh/marad/chi/truffle/nodes/expr/ReadVariableExpr.java
@@ -2,7 +2,10 @@ package gh.marad.chi.truffle.nodes.expr;
 
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import gh.marad.chi.truffle.ChiTypesGen;
 import gh.marad.chi.truffle.nodes.ChiNode;
+import gh.marad.chi.truffle.nodes.value.LambdaValue;
+import gh.marad.chi.truffle.runtime.ChiFunction;
 import gh.marad.chi.truffle.runtime.LexicalScope;
 import gh.marad.chi.truffle.runtime.TODO;
 
@@ -18,6 +21,9 @@ public class ReadVariableExpr extends ChiNode {
     @Override
     public Object executeGeneric(VirtualFrame frame) {
         var value = scope.getValue(name);
+        if (ChiTypesGen.isChiFunction(value)) {
+            replace(new LambdaValue((ChiFunction) value));
+        }
         if (value == null) {
             CompilerDirectives.transferToInterpreter();
             throw new TODO("Undefined name");
