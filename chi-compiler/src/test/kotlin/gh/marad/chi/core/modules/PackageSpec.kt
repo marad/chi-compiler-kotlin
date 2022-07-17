f995c618a592537e4196b93bbdce7977268c6828
Marcin Radoszewski <moriturius@gmail.com>
2022-07-17 22:03:39 +0200

    Experimental package and import specs

---
+++ chi-compiler/src/test/kotlin/gh/marad/chi/core/modules/PackageSpec.kt
@@ -0,0 +1,28 @@
+package gh.marad.chi.core.modules
+
+import gh.marad.chi.core.Compiler
+import io.kotest.core.spec.style.FunSpec
+
+class PackageSpec : FunSpec({
+    test("should set current module and package") {
+        val source = """
+            package std/system
+            val millis = fn() { 0 }
+        """.trimIndent()
+        val result = Compiler.compile(source)
+
+        result.messages.forEach { msg ->
+            System.err.println(Compiler.formatCompilationMessage(source, msg))
+        }
+
+        assert(!result.hasErrors()) { "Compilation errors" }
+    }
+
+    test("allow using commas in module names") {
+        TODO()
+    }
+
+    test("allow using commas in package names") {
+        TODO()
+    }
+})
