08ae64c4f07982a3e3f6efee9740ccb9382ee000
Marcin Radoszewski <moriturius@gmail.com>
2022-08-07 06:58:26 +0200

    Change internal string representation to TruffleString (#19)

--- chi-truffle-language/src/main/java/gh/marad/chi/truffle/nodes/value/StringValue.java
+++ chi-truffle-language/src/main/java/gh/marad/chi/truffle/nodes/value/StringValue.java
@@ -1,16 +1,18 @@
 package gh.marad.chi.truffle.nodes.value;
 
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.strings.TruffleString;
 
 public class StringValue extends ValueNode {
-    private final String value;
+    private final TruffleString value;
 
     public StringValue(String value) {
-        this.value = value;
+        this.value = TruffleString.fromJavaStringUncached(
+                value, TruffleString.Encoding.UTF_8);
     }
 
     @Override
-    public String executeString(VirtualFrame frame) {
+    public TruffleString executeString(VirtualFrame frame) {
         return value;
     }
 
