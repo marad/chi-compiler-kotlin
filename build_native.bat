3175201681394e7871b28372945936df3d176e83
Marcin Radoszewski <moriturius@gmail.com>
2022-08-25 08:31:22 +0200

    Setup simple reflective abilities for interop in native image

--- build_native.bat
+++ build_native.bat
@@ -8,8 +8,12 @@ REM %GRAALVM_HOME%\bin\native-image -H:+ReportUnsupportedElementsAtRuntime --mac
 call gradlew.bat clean chi-truffle-language:shadowJar chi-launcher:jar
 %GRAALVM_HOME%\bin\native-image ^
     -H:+ReportUnsupportedElementsAtRuntime ^
+    -H:ReflectionConfigurationFiles=native/reflectionconfig.json ^
     --macro:truffle ^
+    --language:nfi ^
     --no-fallback ^
+    --enable-http ^
+    --enable-https ^
     -cp chi-truffle-language\build\libs\chi-truffle-language-1.0-all.jar;chi-launcher\build\libs\chi-launcher-1.0.jar ^
     -H:Class=gh.marad.chi.launcher.MainKt ^
     -H:Name=chi ^
