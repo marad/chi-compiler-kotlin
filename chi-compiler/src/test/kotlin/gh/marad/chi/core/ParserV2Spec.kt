04429888c7e832d7c9b2d8ff2c6ad79846ed4d8e
Marcin Radoszewski <moriturius@gmail.com>
2022-09-26 11:57:55 +0200

    Move VariantType parsing tests out from ParserV2Spec.kt

--- chi-compiler/src/test/kotlin/gh/marad/chi/core/ParserV2Spec.kt
+++ chi-compiler/src/test/kotlin/gh/marad/chi/core/ParserV2Spec.kt
@@ -98,62 +98,6 @@ class ParserV2Spec : FunSpec({
         }
     }
 
-    test("parse variant type definition") {
-        val code = "data Result[V, E] = Ok(value: V) | Err(error: E)"
-        val ast = testParse(code)
-        ast shouldHaveSize 1
-        ast[0].shouldBeTypeOf<ParseVariantTypeDefinition>() should {
-            it.typeName shouldBe "Result"
-            it.typeParameters.map { it.name } shouldBe listOf("V", "E")
-
-            it.variantConstructors shouldHaveSize 2
-            it.variantConstructors[0] should { constructor ->
-                constructor.name shouldBe "Ok"
-                constructor.formalArguments shouldHaveSize 1
-                constructor.formalArguments[0].should {
-                    it.name shouldBe "value"
-                    it.typeRef.shouldBeTypeOf<TypeNameRef>()
-                        .typeName.shouldBe("V")
-                }
-            }
-
-            it.variantConstructors[1] should { constructor ->
-                constructor.name shouldBe "Err"
-                constructor.formalArguments shouldHaveSize 1
-                constructor.formalArguments[0].should {
-                    it.name shouldBe "error"
-                    it.typeRef.shouldBeTypeOf<TypeNameRef>()
-                        .typeName.shouldBe("E")
-                }
-            }
-        }
-    }
-
-    test("parse simplified variant type definition") {
-        val code = "data Test[T, U](t: T, u: U)"
-        val ast = testParse(code)
-        ast shouldHaveSize 1
-        ast[0].shouldBeTypeOf<ParseVariantTypeDefinition>() should {
-            it.typeName shouldBe "Test"
-            it.typeParameters.map { it.name } shouldBe listOf("T", "U")
-
-            it.variantConstructors shouldHaveSize 1
-            it.variantConstructors[0] should { constructor ->
-                constructor.name shouldBe "Test"
-                constructor.formalArguments[0].should {
-                    it.name shouldBe "t"
-                    it.typeRef.shouldBeTypeOf<TypeNameRef>()
-                        .typeName.shouldBe("T")
-                }
-                constructor.formalArguments[1].should {
-                    it.name shouldBe "u"
-                    it.typeRef.shouldBeTypeOf<TypeNameRef>()
-                        .typeName.shouldBe("U")
-                }
-            }
-        }
-    }
-
     test("parsing when expression") {
         val code = """
             when {
