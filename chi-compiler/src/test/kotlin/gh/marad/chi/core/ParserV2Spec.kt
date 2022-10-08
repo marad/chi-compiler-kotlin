17e9dc3eb91aad02d00f40678eda41ff5abffc37
Marcin Radoszewski <moriturius@gmail.com>
2022-10-08 21:11:41 +0200

    Move ImportReader tests

--- chi-compiler/src/test/kotlin/gh/marad/chi/core/ParserV2Spec.kt
+++ chi-compiler/src/test/kotlin/gh/marad/chi/core/ParserV2Spec.kt
@@ -31,29 +31,6 @@ fun testParse(code: String): List<ParseAst> {
 @Suppress("unused")
 class ParserV2Spec : FunSpec({
 
-    test("parse import definition") {
-        val code = "import some.module/some.pkg as pkgAlias { foo as fooAlias, bar as barAlias }"
-        val ast = testParse(code)
-        ast shouldHaveSize 1
-        ast[0].shouldBeTypeOf<ParseImportDefinition>() should {
-            it.moduleName.name shouldBe "some.module"
-            it.packageName.name shouldBe "some.pkg"
-            it.packageAlias?.alias shouldBe "pkgAlias"
-            it.entries shouldHaveSize 2
-            it.entries[0] should { fooEntry ->
-                fooEntry.name shouldBe "foo"
-                fooEntry.alias?.alias shouldBe "fooAlias"
-                fooEntry.section?.getCode() shouldBe "foo as fooAlias"
-            }
-            it.entries[1] should { barEntry ->
-                barEntry.name shouldBe "bar"
-                barEntry.alias?.alias shouldBe "barAlias"
-                barEntry.section?.getCode() shouldBe "bar as barAlias"
-            }
-            it.section?.getCode() shouldBe code
-        }
-    }
-
     test("parsing when expression") {
         val code = """
             when {
